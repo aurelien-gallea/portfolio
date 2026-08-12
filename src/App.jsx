98b12895cf06f7deff6995182459210746508ddd
aurelien-gallea <aurelien.gallea@laplateforme.io>
2026-08-12 19:14:30 +0200

    fix: reset scroll position to top on page change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import PortfolioApp from './PortfolioApp';
 import CinetechApp from './cinetech/App';
 import PkmnApp from './pkmnfinder/App';
@@ -8,6 +9,7 @@ import SurvivalGameApp from './survival-game/App';
 function App() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/*" element={<PortfolioApp />} />
         <Route path="/cinetech-react/*" element={<CinetechApp />} />
